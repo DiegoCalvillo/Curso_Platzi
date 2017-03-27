01218bda0e7594f12f8e57702b140c60f827c29b
Diego Calvillo <diegocalvillordz@gmail.com>
2017-03-27 15:00:27 -0600

    Modificación de static

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 
 app.set('view engine', 'pug');
 
+app.use(express.static('public')); //Para que cualquier usuario pueda acceder a los archivos 
+//que se encuentran en public
+
 app.get('/', function(req, res) {
   res.render('index');
 })
